03fb6c7878daefd9017afb23e76a0a056483d185
nirav2005 <73229675+nirav2005@users.noreply.github.com>
2020-10-25 12:04:30 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+
+var monkey , monkey_running;
+var banana ,bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup;
+var score;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  var suvivalTime=0;
+  
+  createCanvas(600,600);
+  
+  monkey=createSprite(80,315,20,20);
+  monkey.addAnimation("moving",monkey_running);
+  monkey.scale=0.1;
+
+  ground=createSprite(400,350,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  console.log(ground.x);
+  
+  FoodGroup=new Group();
+  obstacleGroup= new Group();
+  
+  score=0;
+}
+
+
+function draw() {
+  background("white");
+  
+  if(ground.x<0) {
+    ground.x=ground.width/2;
+  }
+  
+  if(keyDown("space")){
+    monkey.velocityY=-12;
+  }
+  monkey.velocityY=monkey.velocityY+0.8;
+  monkey.collide(ground); 
+  
+  spawnFood();
+  spawnObstacle();
+
+drawSprites();  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: "+score,500,50);
+  
+  stroke("black");
+  textSize(20);
+  fill("black");
+  survivalTime=Math.ceil(frameCount/frameRate())
+  text("Survival Time: "+survivalTime,100,50);
+}
+
+function spawnFood(){
+  if(frameCount%80===0){
+    banana=createSprite(600,280,40,10);
+    banana.y=random(120,200);
+    banana.addImage(bananaImage);
+    banana.scale=0.1;
+    banana.velocityX=-5;
+    
+    banana.lifetime=300;
+    monkey.depth=banana.depth+1;
+    
+    FoodGroup.add(banana);
+  }
+}
+
+function spawnObstacle(){
+  if(frameCount%300===0){
+    obstacle=createSprite(800,320,10,40);
+    obstacle. velocityX=-6;
+    obstacle.addImage(obstacleImage);
+    obstacle.scale=0.10;
+    obstacle.lifetime=300;
+    
+    obstacleGroup.add(obstacle);
+    
+  }
+}
+
+
+
